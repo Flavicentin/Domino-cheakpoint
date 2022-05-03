b20622bb21b55bea139f9a80ab501bed589d9b72
Logon Aluno <logonrmlocal@fiap.com.br>
2022-05-03 11:23:18 -0300

    Update Domino.java

--- src/Domino.java
+++ src/Domino.java
@@ -1,22 +1,26 @@
 
 public class Domino {
-	private Pedra[][] pedras;
+	private Pedra[] pedras;
 
 	public Domino() {
 		int contador = 0;
-		this.pedras = new Pedra[0][26];
+		this.pedras = new Pedra[26];
 		for (int x = 6; x >= 0; x--) {
 			for (int y = 0; y <= 6 - contador; y++) {
-				System.out.println(pedras[x][y]);
-				pedras[x][y] = new Pedra(x, y);
+				pedras[contador] = new Pedra(x, y);
 			}
 			contador++;
 		}
 	}
 	
 	public void imprime() {
-		for(Pedra[] c : pedras) {
+		for(Pedra c : pedras) {
 			System.out.println(c);
 		}	
+	}
+	
+	public static void main(String[] args) {
+		Domino maco = new Domino();
+		maco.imprime();
 	}
 }
